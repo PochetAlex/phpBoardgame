05ddd7c0f707a1a98ca1a7d6b7545006c5dace67
Alex Pochet <thypux@gmail.com>
2023-12-26 14:36:27 +0100

    :sparkles: Affichage détails jeux, formulaire d'ajout d'un jeu et de suppression d'un jeu #2 #6

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,22 +13,70 @@
             padding: 5px;
         }
     </style>
+    <script>
+        function confirmDelete(id) {
+            if (confirm('Voulez-vous vraiment supprimer ce jeu ?')) {
+                document.getElementById(`deleteForm_${id}`).submit();
+            }
+        }
+    </script>
 </head>
 <body>
+
 <h2>La liste des jeux</h2>
+
+<form method="GET" action="{{ route('filterGames') }}">
+    @csrf
+    <button type="submit">Afficher les jeux contenant uniquement un "a"</button>
+</form>
+
+<h2>Ajouter un nouveau jeu</h2>
+<form method="POST" action="{{ route('addGame') }}">
+    @csrf
+    <label for="name">Nom du jeu:</label>
+    <input type="text" id="name" name="name"><br><br>
+
+    <label for="price">Prix:</label>
+    <input type="text" id="price" name="price"><br><br>
+
+    <label for="number_gamer">Nombre de joueurs:</label>
+    <input type="text" id="number_gamer" name="number_gamer"><br><br>
+
+    <label for="playing_time">Temps de jeu:</label>
+    <input type="text" id="playing_time" name="playing_time"><br><br>
+
+    <label for="complexity">Complexité:</label>
+    <input type="text" id="complexity" name="complexity"><br><br>
+
+    <label for="category">Catégorie:</label>
+    <input type="text" id="category" name="category"><br><br>
+
+    <label for="published_date">Date de publication:</label>
+    <input type="text" id="published_date" name="published_date"><br><br>
+
+    <label for="description">Description:</label><br>
+    <textarea id="description" name="description" rows="4" cols="50"></textarea><br><br>
+
+    <button type="submit">Ajouter le jeu</button>
+</form>
+
 
 @if(!empty($jeux))
     <table>
         <thead>
         <tr>
             <td>ID</td>
-            <td>name</td>
-            <td>description</td>
-            <td>price</td>
-            <td>number_gamer</td>
-            <td>playing_time</td>
-            <td>complexity</td>
-            <td>rating</td>
+            <td>Nom</td>
+            <td>Description</td>
+            <td>Prix</td>
+            <td>Nombre de joueurs</td>
+            <td>Temps de jeu</td>
+            <td>Complexité</td>
+            <td>Évaluation</td>
+            <td>Catégorie</td>
+            <td>Date de publication</td>
+            <td>Détails jeu</td>
+            <td>Suppression</td>
         </tr>
         </thead>
         <tbody>
@@ -42,14 +90,25 @@
                 <td>{{$jeu["playing_time"]}}</td>
                 <td>{{$jeu["complexity"]}}</td>
                 <td>{{$jeu["rating"]}}</td>
+                <td>{{$jeu["category"]}}</td>
+                <td>{{$jeu["published_date"]}}</td>
+                <td><a href="{{ route('showGame', ['id' => $jeu['id']]) }}">Voir détails</a></td>
+                <td>
+                    <!-- Formulaire pour chaque jeu avec un bouton de suppression -->
+                    <form id="deleteForm_{{ $jeu['id'] }}" method="POST" action="{{ route('deleteGame', ['id' => $jeu['id']]) }}">
+                        @csrf
+                        @method('DELETE')
+                        <button type="button" onclick="confirmDelete({{ $jeu['id'] }})">
+                            Supprimer
+                        </button>
+                    </form>
+                </td>
             </tr>
-
         @endforeach
         </tbody>
     </table>
-
 @else
-    <h3>aucun jeu</h3>
+    <h3>Aucun jeu</h3>
 @endif
 
 </body>
